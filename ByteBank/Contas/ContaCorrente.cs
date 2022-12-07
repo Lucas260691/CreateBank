1cc50d2c84f70ddf45de282cd59bf2f3e4b17ef3
Lucas Sodre <lucasmeinen@yahoo.com.br>
2022-12-07 18:13:04 -0300

    Criando a propriedade Numero_agencia

--- ByteBank/Contas/ContaCorrente.cs
+++ ByteBank/Contas/ContaCorrente.cs
@@ -9,11 +9,21 @@ namespace ByteBank.Contas
 {
     public class ContaCorrente
     {
-        public int numero_agencia;
-        public string conta;
+        private int numero_agencia;
+        public int Numero_agencia
+        {
+            get { return this.numero_agencia; }
+            set {
+                    if (value > 0)
+                    {
+                        this.numero_agencia = value;
+                    }
+                }
+        }
+        private string conta;
         private double saldo = 100;
 
-        public Cliente titular;
+        private Cliente titular;
 
         public void Depositar(double valor)
         {
